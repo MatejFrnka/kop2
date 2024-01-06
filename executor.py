69a02aa8c15a59b4baa261611a3906cf2fc7277e
Matěj Frnka <mat.frnka@gmail.com>
2024-01-06 21:21:12 +0100

    executor updated

--- executor.py
+++ executor.py
@@ -1,4 +1,5 @@
 import os
+import time
 from pathlib import Path
 import random
 
@@ -7,6 +8,8 @@ from matplotlib import pyplot as plt
 
 from main import GeneticAlgorithm, Gene
 from utility import parse_input
+
+from tqdm import tqdm
 
 
 class Executor:
@@ -35,8 +38,8 @@ class Executor:
 
     def execute(self, genetic_algorithm: GeneticAlgorithm, repeat=1, limit=None):
         files = os.listdir(self.instances_folder_path())
-        for i, file in enumerate(files):
-            if limit < i:
+        for i, file in tqdm(enumerate(files), desc="Files", position=0, leave=True):
+            if limit <= i:
                 break
             self.execute_file(file, genetic_algorithm, repeat)
 
@@ -45,16 +48,20 @@ class Executor:
         input_str = open(file_path, 'r').read()
         c, w = parse_input(input_str)
         for i in range(repeat):
+            tic = time.time()
             found_gene: Gene = genetic_algorithm.run(c, w)
+            toc = time.time() - tic
 
-            log_df = pd.DataFrame(ga.log)
+            log_df = pd.DataFrame(genetic_algorithm.log)
             log_df['it'] = i
             log_df['category'] = self.dataset_type
             log_df['dataset_name'] = self.dataset_name
             log_df['dataset_file'] = file
+            log_df['time'] = toc
+            log_df['solution_weights'] = self.solution(file)[0]
 
-            for cnf in ga.get_config().keys():
-                log_df[f"config_{cnf}"] = ga.get_config()[cnf]
+            for cnf in genetic_algorithm.get_config().keys():
+                log_df[f"config_{cnf}"] = genetic_algorithm.get_config()[cnf]
 
             self.data = pd.concat([self.data, log_df], ignore_index=True)
 
@@ -62,28 +69,39 @@ class Executor:
             if self.verbose:
                 print(f"{file}: Found: {found}, Best: {self.solution(file)}")
             if self.plots:
-                plt.plot([p['best_gene_fitness'] for p in ga.log])
+                plt.plot([p['best_gene_fitness'] for p in genetic_algorithm.log])
                 plt.title(f"{i}, {file}: Found: {found}, Best: {self.solution(file)}")
                 plt.show()
 
 
-if __name__ == "__main__":
+def measure():
     # random.seed(4)
-    e = Executor('wuf20-91', 'M', verbose=True)
-    ga = GeneticAlgorithm(population_size=800, initial_mutation_rate=0.4, mutation_scaler=0.97, generations=5,
-                          elites=0.01, verbose=False)
-    e.execute(ga, 1, limit=5)
-    e.data.to_csv("wuf20-91_test.csv")
-    # path = 'wu/f20-91/wuf20-91-M/wuf20-01.mwcnf'
-    # input_str = open(path, 'r').read()
-    # c, w = parse_input(input_str)
-    # best = [0 if int(f) < 0 else 1 for f in
-    #         "1 -2 -3 4 -5 6 -7 -8 -9 10 -11 -12 13 14 15 -16 17 -18 -19 20".split(" ")]
-    # print(best)
-    # print(evaluate(c, w, best))
-    #
+    datasets = ['wuf20-91']
+    problems = ['M', 'N', 'Q', 'R']
+    algos = [
+        GeneticAlgorithm(population_size=500, initial_mutation_rate=0.15, mutation_scaler=1, generations=60,
+                         elites=0, verbose=False)
+    ]
 
-    # ga.run()
-    # print(ga.best_valid)
-    # print(f"Weights: {ga.best_valid.weights_sum}")
-    #
+    df_all = pd.DataFrame()
+
+    repeat = 6
+    limit = 5
+    with tqdm(total=len(datasets) * len(problems) * len(algos), desc='Overall Progress') as progress_bar:
+        for dataset in datasets:
+            for problem in problems:
+                for ga in algos:
+                    e = Executor('wuf20-91', 'M', verbose=False)
+                    e.execute(ga, repeat, limit)
+                    e.data.to_csv(f"{dataset}_{problem}_{time.time()}.csv")
+                    df_all = pd.concat([df_all, e.data], ignore_index=True)
+                    progress_bar.update(1)
+    df_all.to_csv(f"{dataset}_{time.time()}.csv")
+
+
+if __name__ == "__main__":
+    measure()
+    # e = Executor('wuf20-91', 'M', verbose=True)
+    # ga = GeneticAlgorithm(population_size=800, initial_mutation_rate=0.1, mutation_scaler=1, generations=80,
+    #                       elites=0.01, verbose=True)
+    # e.execute(ga, 1, limit=1)
